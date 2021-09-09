943e6f8c1e400c71c833077c35858ae2bc0cd818
vunn-2690 <nguyen.nam.vu@sun-asterisk.com>
2021-09-09 15:25:40 +0700

    Chapter 3: Mostly static pages
    fix conflict by rebase master
    
     fix conflict after rubocop
    
    fix error by rubocop
    
    fix comment
    
    fix comment 2

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :vi
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|vi/ do
+    get "static_pages/home"
+    get "static_pages/help"
+    get "static_pages/contact"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
