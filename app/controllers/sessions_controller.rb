956bb7b6d98258995ec7374f5f865b0a89119eb0
vunn-2690 <nguyen.nam.vu@sun-asterisk.com>
2021-09-16 17:03:45 +0700

    Chapter 9: Advance Login
    fix comment v1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by email: params[:session][:email].downcase
-    if user&.authenticate params[:session][:password]
+    if user.try(:authenticate, params[:session][:password])
       log_in user
+      check_remember user
       redirect_to user
     else
       flash.now[:danger] = t "shared.invalid_email_password_combination"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
   validates :name, presence: true, length: {minimum: Settings.length.min_10}
@@ -15,5 +16,22 @@ class User < ApplicationRecord
               BCrypt::Engine.cost
             end
     BCrypt::Password.create string, cost: cost
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_column :remember_digest, nil
   end
 end
