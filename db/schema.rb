cb38b8fd3d5574d37e67f937858de7c195f3b4df
vunn-2690 <nguyen.nam.vu@sun-asterisk.com>
2021-09-21 13:10:37 +0700

    Chapter 12: Reset Password
    fix comment v1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_20_064332) do
+ActiveRecord::Schema.define(version: 2021_09_21_032920) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2021_09_20_064332) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
 end
